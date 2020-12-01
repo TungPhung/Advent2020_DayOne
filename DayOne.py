ac032ffe0a79d283217aabe52daa189e4316b9ba
Tung Phung <ttp2d@virginia.edu>
2020-12-01 03:34:46 -0500

    Initial commit

---
+++ DayOne.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os,sys
+
+test_input = [1721, 979, 366, 299, 675, 1456]
+
+def read_file():
+    all = []
+    with open('input.txt', 'r') as f:
+        for i in f:
+            all.append(int(i.strip('\n')))
+    return all
+
+def find_2020(array):
+    twenty_twenty_list = [2020 - i for i in array]
+    for i in twenty_twenty_list:
+        for j in array:
+            if i == j:
+                print(i,2020-i)
+                return i*(2020-i)
+
+if __name__ == "__main__":
+    print(find_2020(read_file()))    
